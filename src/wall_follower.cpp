d804202d3f718a1a7209415fdf91d801e766ee8a
Gundars <gundars@gkPC.(none)>
2014-10-19 12:52:24 +0200

    changed to PID

--- src/wall_follower.cpp
+++ src/wall_follower.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "ras_arduino_msgs/ADConverter.h"
+#include "ras_utils/controller.h"
 
 #define PUBLISH_RATE 10 // Hz
 #define QUEUE_SIZE 1000
@@ -17,9 +18,9 @@ private:
     int adc_front;
     int adc_back;
 
-    double gain;
-    double linear_speed;
-    double angular_speed;
+    // double gain;
+    double v;
+    double w;
     // In which side is wall
     bool wall_is_left;
 
@@ -28,6 +29,9 @@ private:
     ros::Subscriber adc_sub_;
     // Callback func when adc data recieved
     void adcCallback(const ras_arduino_msgs::ADConverter::ConstPtr& msg);
+
+    Controller controller_w;
+    double kp_w, ki_w, kd_w;
 };
 
 int main (int argc, char* argv[])
@@ -49,7 +53,7 @@ Wall_follower::Wall_follower(const ros::NodeHandle &n)
     // initial values
     adc_front = 0;
     adc_back = 0;
-    angular_speed = 0;
+    w = 0;
 
     // Publisher
     twist_pub_ = n_.advertise<geometry_msgs::Twist>("/motor_controller/twist", QUEUE_SIZE);
@@ -57,8 +61,12 @@ Wall_follower::Wall_follower(const ros::NodeHandle &n)
     adc_sub_ = n_.subscribe("/arduino/adc", 1,  &Wall_follower::adcCallback, this);
 
     // params from launch file
-    n_.getParam("Wall_follower/gain", gain);
-    n_.getParam("Wall_follower/linear_speed", linear_speed);
+    n_.getParam("Wall_follower/W/KP", kp_w);
+    n_.getParam("Wall_follower/W/KD", kd_w);
+    n_.getParam("Wall_follower/W/KI", ki_w);
+
+    // n_.getParam("Wall_follower/gain", gain);
+    n_.getParam("Wall_follower/linear_speed", v);
     n_.getParam("Wall_follower/wall_is_left", wall_is_left);
 }
 
@@ -67,24 +75,32 @@ void Wall_follower::run()
     // ** Publish data
     ros::Rate loop_rate(PUBLISH_RATE);
 
+    int delta;
     while(ros::ok())
     {
+        delta = adc_back - adc_front;
         if(wall_is_left)
-            angular_speed = gain * ( adc_back - adc_front );
+        {
+            controller_w.setData(0, delta);
+            w = controller_w.computeControl();
+        }
         else
-            angular_speed = gain * ( adc_front - adc_back );
+        {
+            controller_w.setData(0, -delta);
+            w = controller_w.computeControl();
+        }
 
         geometry_msgs::Twist msg;
 
-        msg.linear.x = linear_speed;
+        msg.linear.x = v;
         msg.linear.y = 0.0;
         msg.linear.z = 0.0;
 
         msg.angular.x = 0.0;
         msg.angular.y = 0.0;
-        msg.angular.z = angular_speed;
+        msg.angular.z = w;
 
-        ROS_INFO("(v,w): %f, %f\n", linear_speed, angular_speed);
+        ROS_INFO("(v,w): %f, %f\n", v, w);
 
         twist_pub_.publish(msg);
 
